6f5b3c797495f3ce7b837a5ab9a475ee24968b56
Rodrigo Comesaña <rcomesan@gmail.com>
2019-04-25 14:28:15 -0300

    Agrego uniquify a módulo sort de cx para eliminar duplicados in-place.

--- cx/include/cx/sort.h
+++ cx/include/cx/sort.h
@@ -2,6 +2,7 @@
 #define CX_SORT_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef int32_t(*cx_sort_comp_cb)(const void* _a, const void* _b);
 
@@ -11,6 +12,8 @@ typedef int32_t(*cx_sort_comp_cb)(const void* _a, const void* _b);
 
 void        cx_sort_quick(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb _comp);
 
-uint32_t    cx_sort_find(void* _data, uint32_t _size, uint32_t _num, const void* _key, cx_sort_comp_cb _comp);
+uint32_t    cx_sort_find(void* _data, uint32_t _size, uint32_t _num, const void* _key, bool _firstMatch, cx_sort_comp_cb _comp);
+
+uint32_t    cx_sort_uniquify(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb _comp, cx_destroyer_cb _destroyer);
 
 #endif // CX_SORT_H_
--- cx/src/sort.c
+++ cx/src/sort.c
@@ -1,3 +1,4 @@
+#include "cx.h"
 #include "sort.h"
 
 #include <string.h>
@@ -22,6 +23,12 @@ void cx_sort_quick(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb _
     // a value greater than zero    if _a should be placed to the right of _b.
     // a value equal to zero        if _a and b are equal.
 
+    CX_CHECK(_size > 0, "_size must be greater than zero!");
+    CX_CHECK_NOT_NULL(_data);
+    CX_CHECK_NOT_NULL(_comp);
+
+    if (0 == _num) return;
+
     void* pivot = malloc(_size);
     void* temp = malloc(_size);
 
@@ -31,15 +38,25 @@ void cx_sort_quick(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb _
     free(temp);
 }
 
-uint32_t cx_sort_find(void* _data, uint32_t _size, uint32_t _num, const void* _key, cx_sort_comp_cb _comp)
+uint32_t cx_sort_find(void* _data, uint32_t _size, uint32_t _num, const void* _key, bool _firstMatch, cx_sort_comp_cb _comp)
 {
-    // searches for the first element that is greater than or equal to the _key parameter using binary search
+    // searches for an element that is greater than or equal to the _key parameter using binary search
     // in the given _data array of _num elements of _size bytes each which is assumed to be sorted.
+    // if _firstMatch is true, and _key is found, the returned value is guaranteed to be the first 
+    // element in the array that matches the given _key.
+
+    CX_CHECK(_size > 0, "_size must be greater than zero!");
+    CX_CHECK_NOT_NULL(_data);
+    CX_CHECK_NOT_NULL(_key);
+    CX_CHECK_NOT_NULL(_comp);
+
+    if (0 == _num) return -1; // element is not found, and it should be placed first in the list
 
     int32_t left = 0;
     int32_t right = _num;
     int32_t mid = 0;
     int32_t result = 0;
+    bool found = false;
     char* data = (char*)_data;
 
     while (left < right)
@@ -47,18 +64,23 @@ uint32_t cx_sort_find(void* _data, uint32_t _size, uint32_t _num, const void* _k
         mid = (left + right) / 2;
         result = _comp(&data[mid * _size], _key);
 
-        if (result >= 0)
-        {
-            right = mid;
-        }
-        else
+        if (result < 0)
         {
             left = mid + 1;
         }
+        else if (result > 0)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            if (!_firstMatch) return mid;
+            found = true;
+            right = mid; // keep searching for the first occurrence
+        }
     }
 
-    result = _comp(&data[left * _size], _key);
-    if (0 == result)
+    if (found)
     {
         return left;
     }
@@ -128,4 +150,36 @@ static void _cx_sort(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb
     {
         _cx_sort(&data[(j + 1) * _size], _size, _num, _comp, _pivot, _temp);
     }
+}
+
+uint32_t cx_sort_uniquify(void* _data, uint32_t _size, uint32_t _num, cx_sort_comp_cb _comp, cx_destroyer_cb _destroyer)
+{
+    // removes duplicates in-place from the sorted _data array of _num elements of 
+    // _size bytes each. comparisons are made using _comp callback, and only 
+    // the first unique occurrence of each element is preserved in the modified array.
+    // you can use the optional parameter _destroyer if your elements contain references
+    // and they must be freed before having their memory overwritten.
+    // the returned value is the new number of elements in the modified _data array.
+
+    CX_CHECK(_size > 0, "_size must be greater than zero!");
+    CX_CHECK_NOT_NULL(_data);
+    CX_CHECK_NOT_NULL(_comp);
+
+    if (0 == _num) return 0;
+
+    uint32_t count = 0;
+
+    for (uint32_t i = 1; i < _num; i++)
+    {
+        if (0 != _comp(&(_data[i * _size]), &(_data[count * _size])))
+        {
+            count++;
+            memcpy(&(_data[count * _size]), &(_data[i * _size]), _size);
+        }
+        else if (NULL != _destroyer)
+        {
+            _destroyer(&(_data[i * _size]));
+        }
+    }
+    return ++count;
 }
